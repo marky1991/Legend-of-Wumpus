6d9db4b68b3e9ad4ee7d66d6a6a75a6c0dd0cb6f
marky1991 <marky1991@gmail.com>
2014-04-11 08:47:44 -0400

    Remove tabs.

--- units.py
+++ units.py
@@ -19,68 +19,68 @@ magic_types = magic_triangle_types | {"light", "dark"}
 class Unit:
     #TODO: Remind me what this is for.
     #What does base_weapons mean again???
-	base_weapons = set()
-	def __init__(self):
-		#Represents the max growth per level. Multipied by a random number between 0 and 1,
-		#then rounded to the nearest integer 
-		self.growth_rates = {"resistance": 0,
-				    	"strength": 0,
-					"speed": 0,
-					"skill": 0,
-					"magic": 0,
-					"luck": 0,
-					"defense": 0,
-					"hp": 0
-					}
+    base_weapons = set()
+    def __init__(self):
+        #Represents the max growth per level. Multipied by a random number between 0 and 1,
+        #then rounded to the nearest integer 
+        self.growth_rates = {"resistance": 0,
+                        "strength": 0,
+                    "speed": 0,
+                    "skill": 0,
+                    "magic": 0,
+                    "luck": 0,
+                    "defense": 0,
+                    "hp": 0
+                    }
 
-		self.resistance = 0
-		self.strength = 0
-		self.speed = 0
-		self.skill = 0
-		self.magic = 0
-		self.luck = 0
-		self.defense = 0
-		self.hp = 10
-		#Weight is between 0 and 10
-		self.weight = 5
-		self.movement = 4
-		#weapon_skill is on a geade scale of F-Ss
-		self.weapon_skill = {"axe": None,
-					"bow": None,
-					"sword": None,
-					#Heal, buff, etc.
-					"staff": None,
-					#Used for heron-style buffs
-					"song": None,
-					"lance": None,
-					"knife": None,
-					#Also includes dragon breath, claws, etc.
-					"hand": None,
-					"fire": None,
-					"wind": None,
-					"lightning": None,
-					"light": None,
-					"dark": None,
-					#For summoning
-					"wand": None
-				}		
+        self.resistance = 0
+        self.strength = 0
+        self.speed = 0
+        self.skill = 0
+        self.magic = 0
+        self.luck = 0
+        self.defense = 0
+        self.hp = 10
+        #Weight is between 0 and 10
+        self.weight = 5
+        self.movement = 4
+        #weapon_skill is on a geade scale of F-Ss
+        self.weapon_skill = {"axe": None,
+                    "bow": None,
+                    "sword": None,
+                    #Heal, buff, etc.
+                    "staff": None,
+                    #Used for heron-style buffs
+                    "song": None,
+                    "lance": None,
+                    "knife": None,
+                    #Also includes dragon breath, claws, etc.
+                    "hand": None,
+                    "fire": None,
+                    "wind": None,
+                    "lightning": None,
+                    "light": None,
+                    "dark": None,
+                    #For summoning
+                    "wand": None
+                }        
 
-	def level_up(self):
-		increased_none = True	
-		while increased_none:
-			for attribute_name, growth_rate in self.growth_rates.items():
-				guaranteed_levels, growth_rate  = divmod(growth_rate, 100)
-				increased_level = random.random() > growth_rate/100
-				increased_none = guaranteed_levels + increased_level == 0 
-				setattr(self, attribute_name, getattr(self, attribute_name) + guaranteed_levels + increased_level)
-	def __str__(self):
-		return "{0}({1})".format(type(self), self.name)
+    def level_up(self):
+        increased_none = True    
+        while increased_none:
+            for attribute_name, growth_rate in self.growth_rates.items():
+                guaranteed_levels, growth_rate  = divmod(growth_rate, 100)
+                increased_level = random.random() > growth_rate/100
+                increased_none = guaranteed_levels + increased_level == 0 
+                setattr(self, attribute_name, getattr(self, attribute_name) + guaranteed_levels + increased_level)
+    def __str__(self):
+        return "{0}({1})".format(type(self), self.name)
 
 class Horseman(Unit):
-	"""Oscar, level ?"""
-	def __init__(self):
+    """Oscar, level ?"""
+    def __init__(self):
         super().__init__()
-		self.growth_rates = {"resistance": 30,
+        self.growth_rates = {"resistance": 30,
                                         "strength": 45,
                                         "speed": 45,
                                         "skill": 50,
@@ -89,13 +89,13 @@ class Horseman(Unit):
                                         "defense": 35,
                                         "hp": 55
                                         }
-		self.hp = 26
-		self.resistance = 0
-		self.magic = 1
-		self.skill = 6
-		self.speed = 7
-		self.luck = 5
-		self.defense = 8
+        self.hp = 26
+        self.resistance = 0
+        self.magic = 1
+        self.skill = 6
+        self.speed = 7
+        self.luck = 5
+        self.defense = 8
 
         #TODO: Discuss whether we want to give level E hand to everyone or not.
         #TODO: Also, discuss if we really want to seperate horsemen by weapon
@@ -110,10 +110,10 @@ class Horseman(Unit):
         self.weapon_skill[preferred_weapon] = "E"
 
 class Myrmidon(Unit):
-	"""Mia, level 6"""
-	def __init__(self):
+    """Mia, level 6"""
+    def __init__(self):
         super().__init__()
-		self.growth_rates = {"resistance": 30,
+        self.growth_rates = {"resistance": 30,
                                         "strength": 45,
                                         "speed": 45,
                                         "skill": 50,
@@ -121,9 +121,8 @@ class Myrmidon(Unit):
                                         "luck": 30,
                                         "defense": 35,
                                         "hp": 55
-                                        }
-	
-		self.hp = 21
+                                        } 
+        self.hp = 21
         self.resistance = 0
         self.magic = 0
         self.skill = 6
